0c05db4a9245965585455a4b33900711a3614bf5
fgiaimo <giaimo@chalmers.se>
2016-08-31 14:32:38 +0200

    Temporary work on back-end [make fails]

--- code/core/system/proxy-fh16truck/include/ProxyFH16Truck.h
+++ code/core/system/proxy-fh16truck/include/ProxyFH16Truck.h
@@ -20,42 +20,89 @@
 #ifndef PROXY_PROXYFH16TRUCK_H
 #define PROXY_PROXYFH16TRUCK_H
 
+#include <fstream>
+#include <map>
+#include <memory>
+#include <string>
+
+#include <opendavinci/odcore/base/FIFOQueue.h>
+#include <opendavinci/odcore/base/Mutex.h>
 #include <opendavinci/odcore/base/module/TimeTriggeredConferenceClientModule.h>
+#include <opendavinci/odcore/reflection/CSVFromVisitableVisitor.h>
+#include <odcantools/GenericCANMessageListener.h>
+
+namespace automotive {
+class GenericCANMessage;
+}
+namespace automotive {
+namespace odcantools {
+class CANDevice;
+}
+}
+namespace odtools {
+namespace recorder {
+class Recorder;
+}
+}
 
 namespace opendlv {
 namespace core {
 namespace system {
 namespace proxy {
 
+class CanMessageDataStore;
+
 using namespace std;
 
 /**
  * Interface to FH16 truck.
  */
-class ProxyFH16Truck : public odcore::base::module::TimeTriggeredConferenceClientModule {
-   private:
-    ProxyFH16Truck(const ProxyFH16Truck & /*obj*/) = delete;
-    ProxyFH16Truck &operator=(const ProxyFH16Truck & /*obj*/) = delete;
+class ProxyFH16Truck : public odcore::base::module::TimeTriggeredConferenceClientModule,
+    public automotive::odcantools::GenericCANMessageListener {
+    public:
+        ProxyFH16Truck(int32_t const &, char **);
+        ProxyFH16Truck(ProxyFH16Truck const &) = delete;
+        ProxyFH16Truck &operator=(ProxyFH16Truck const &) = delete;
+        virtual ~ProxyFH16Truck();
 
-   public:
-    /**
-     * Constructor.
-     *
-     * @param argc Number of command line arguments.
-     * @param argv Command line arguments.
-     */
-    ProxyFH16Truck(const int &argc, char **argv);
+        virtual void nextGenericCANMessage(const automotive::GenericCANMessage &gcm);
 
-    virtual ~ProxyFH16Truck();
+    private:
+        void setUp();
+        void tearDown();
+        odcore::data::dmcp::ModuleExitCodeMessage::ModuleExitCode body();
 
-   private:
-    void setUp();
-    void tearDown();
-    odcore::data::dmcp::ModuleExitCodeMessage::ModuleExitCode body();
+        void setUpRecordingGenericCANMessage(const std::string &timeStampForFileName);
+        void setUpRecordingMappedGenericCANMessage(const std::string &timeStampForFileName);
+
+        odcore::data::Container addCANTimeStamp(odcore::data::Container &c, const odcore::data::TimeStamp &ts);
+        void dumpASCData(const automotive::GenericCANMessage &gcm);
+        void dumpCSVData(odcore::data::Container &c);
+
+        // method from opendlv disabled as not relevant for ps3controller
+        //void handleBeacons();
+
+        void disableCANRequests();
+
+        odcore::base::FIFOQueue m_fifoGenericCanMessages;
+        std::unique_ptr<odtools::recorder::Recorder> m_recorderGenericCanMessages;
+
+        odcore::base::FIFOQueue m_fifoMappedCanMessages;
+        std::unique_ptr<odtools::recorder::Recorder> m_recorderMappedCanMessages;
+
+        std::shared_ptr<automotive::odcantools::CANDevice> m_device;
+        std::unique_ptr<CanMessageDataStore> m_canMessageDataStore;
+
+        canmapping::CanMapping m_revereFh16CanMessageMapping;
+
+        odcore::data::TimeStamp m_startOfRecording;
+        std::shared_ptr<std::fstream> m_ASCfile;
+        std::map<uint32_t, std::shared_ptr<std::fstream>> m_mapOfCSVFiles;
+        std::map<uint32_t, std::shared_ptr<odcore::reflection::CSVFromVisitableVisitor>> m_mapOfCSVVisitors;
 };
-}
-}
-}
+} // proxy
+} // system
+} // core
 } // opendlv::core::system::proxy
 
 #endif /*PROXY_PROXYFH16TRUCK_H*/
