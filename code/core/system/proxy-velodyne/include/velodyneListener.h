a2505bc720e0de9a6cbdd1deb6c88065f1cb131d
HangYin2015 <young814young814@gmail.com>
2016-09-21 11:17:51 +0200

    Remove tabs

--- code/core/system/proxy-velodyne/include/velodyneListener.h
+++ code/core/system/proxy-velodyne/include/velodyneListener.h
@@ -65,9 +65,9 @@ namespace proxy {
                 virtual void nextContainer(odcore::data::Container &c);
                 
             private:
-	            const uint32_t MAX_POINT_SIZE=101000;  //The maximum number of points per frame is set based on the observation of the first 100 frames of the pcap file imeangowest.pcap. This upper bound should be set as low as possible, as it affects the shared memory size and thus the frame updating speed.
-	            const uint32_t SIZE_PER_COMPONENT = sizeof(float);
-	            const uint8_t NUMBER_OF_COMPONENTS_PER_POINT = 4; // How many components do we have per vector?
+                const uint32_t MAX_POINT_SIZE=101000;  //The maximum number of points per frame is set based on the observation of the first 100 frames of the pcap file imeangowest.pcap. This upper bound should be set as low as possible, as it affects the shared memory size and thus the frame updating speed.
+                const uint32_t SIZE_PER_COMPONENT = sizeof(float);
+                const uint8_t NUMBER_OF_COMPONENTS_PER_POINT = 4; // How many components do we have per vector?
                 const uint32_t SIZE = MAX_POINT_SIZE * NUMBER_OF_COMPONENTS_PER_POINT * SIZE_PER_COMPONENT; // What is the total size of the shared memory?    
                 const float PI=3.14159;
                 
@@ -83,10 +83,10 @@ namespace proxy {
                 odcore::io::conference::ContainerConference& velodyneFrame;
                 odcore::data::SharedPointCloud spc;
                 float rotCorrection[64];
-	            float vertCorrection[64];
-	            float distCorrection[64];
-	            float vertOffsetCorrection[64];
-	            float horizOffsetCorrection[64];                
+                float vertCorrection[64];
+                float distCorrection[64];
+                float vertOffsetCorrection[64];
+                float horizOffsetCorrection[64];                
         };
 
 }
